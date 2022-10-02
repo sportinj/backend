0d38133527d86dbd83c78b798da9b5e08ec9a099
Alexandr Beloded <105494913+Beloded1@users.noreply.github.com>
2022-10-02 18:40:06 +0400

    Add some fix

--- backend/teams.py
+++ backend/teams.py
@@ -1,36 +1,62 @@
 from flask import Flask, jsonify, request
 from http import HTTPStatus
 from uuid import uuid4
+from typing import Any
 
 app = Flask(__name__)
 
+Json = dict[int, str|int]
+class Storage:
+    def __init__(self):
+        self.teams: dict[int, Json] = {}
+        self.last_uid = 0
 
-teams = []
+    def add(self, team: Json) -> Json :
+        self.last_uid +=1
+        team["uid"] = self.last_uid
+        self.teams[self.last_uid] = team
+        return team
+
+    def get_teams(self) -> list[Json]:
+        return list(self.teams.values())
+
+    def get_team_by_id(self, uid:int) ->Json:
+        return self.teams[uid]
+
+    def update_by_id(self, uid:int, team: Json) ->Json:
+        self.teams[uid] = team
+        return team
+
+    def delete(self, uid: int) -> None:
+        self.teams.pop(uid)
+
+
+
+
+storage = Storage()
+
 
 @app.post('/api/teams/')
 def add_team():
-    payload = request.json
-    payload["uid"] = len(teams)+1
-    teams.append(payload)
-    return payload, 201
+    team = request.json
+    return storage.add(team), 201
 
 @app.get('/api/teams/')
 def get_teams():
-    return teams, 200
+    return storage.get_teams(), 200
 
 @app.get('/api/teams/<int:uid>')
 def get_team_by_id(uid):
-    return teams[uid-1], 200
+    return storage.get_team_by_id(uid), 200
 
 @app.put('/api/teams/<int:uid>')
 def update_by_id(uid):
-    payload = request.json
-    for key in payload:
-        teams[uid][key] = payload[key]
-    return teams[uid]
+    team = request.json
+    return storage.update_by_id(uid, team), 200
 
 
 @app.delete('/api/teams/<int:uid>')
 def delete_team(uid):
-    return teams.pop(uid), 201
+    storage.delete(uid)
+    return {}, 204
 
