aac1a41dcfce29c671f2ca9e0c36b97840c4d37c
Alexandr Beloded <105494913+Beloded1@users.noreply.github.com>
2022-10-04 21:32:47 +0400

    Add ValidationError to add_team handler

--- backend/teams.py
+++ backend/teams.py
@@ -2,28 +2,36 @@ from flask import Flask, jsonify, request
 from http import HTTPStatus
 from uuid import uuid4
 from typing import Any
+from pydantic import BaseModel, ValidationError
+
+
 
 app = Flask(__name__)
 
 Json = dict[int, str|int]
-class Storage:
+
+class Team(BaseModel):
+    uid: int
+    name: str
+
+class LocalStorage:
     def __init__(self):
-        self.teams: dict[int, Json] = {}
+        self.teams: dict[int, Team] = {}
         self.last_uid = 0
 
-    def add(self, team: Json) -> Json :
+    def add(self, team: Team) -> Team :
         self.last_uid +=1
-        team["uid"] = self.last_uid
+        team.uid = self.last_uid
         self.teams[self.last_uid] = team
         return team
 
-    def get_teams(self) -> list[Json]:
+    def get_teams(self) -> list[Team]:
         return list(self.teams.values())
 
-    def get_team_by_id(self, uid:int) ->Json:
+    def get_team_by_id(self, uid:int) ->Team:
         return self.teams[uid]
 
-    def update_by_id(self, uid:int, team: Json) ->Json:
+    def update_by_id(self, uid:int, team: Team) ->Team:
         self.teams[uid] = team
         return team
 
@@ -33,13 +41,20 @@ class Storage:
 
 
 
-storage = Storage()
+storage = LocalStorage()
 
 
 @app.post('/api/teams/')
 def add_team():
-    team = request.json
-    return storage.add(team), 201
+    payload = request.json
+    payload["uid"] = -1
+    try:
+        team = Team(**payload)
+    except ValidationError as err:
+        return {"error": str(err)}, 400
+
+    team = storage.add(team)
+    return team.dict(), 201
 
 @app.get('/api/teams/')
 def get_teams():
